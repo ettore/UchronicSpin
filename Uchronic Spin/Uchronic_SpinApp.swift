f02409111c65419c8b19e310240807a4cfb432c7
Ettore Pasquini <ettorepasquini@icloud.com>
2025-01-04 15:01:57 -0800

    Initial Commit

---
+++ Uchronic Spin/Uchronic_SpinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uchronic_SpinApp.swift
+//  Uchronic Spin
+//
+//  Created by Ettore Pasquini on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Uchronic_SpinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Uchronic SpinTests/Uchronic_SpinTests.swift
@@ -0,0 +1,17 @@
+//
+//  Uchronic_SpinTests.swift
+//  Uchronic SpinTests
+//
+//  Created by Ettore Pasquini on 1/4/25.
+//
+
+import Testing
+@testable import Uchronic_Spin
+
+struct Uchronic_SpinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
